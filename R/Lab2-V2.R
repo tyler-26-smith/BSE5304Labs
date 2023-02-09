2efdf43830259c54f0966d8d4ee1fb46c5cee8b4
tyler-26-smith <tylersmith22@vt.edu>
2023-02-08 21:08:19 -0500

    scratch

--- R/Lab2-V2.R
+++ R/Lab2-V2.R
@@ -32,9 +32,9 @@ pacman::p_load(EcoHydRology)
 
 setwd(datadir)
 # Get some flow data from USGS 0205551460 `
-myflowgage_id="0205551460"
+myflowgage_id="01673550"
 myflowgage=get_usgs_gage(myflowgage_id,begin_date = "2015-01-01",
-                         end_date = "2019-01-01")
+                         end_date = "2023-02-07")
 
 # For most watershed modelling purposes we normalize Q in mm/day for basins
 myflowgage$flowdata$Qmm = myflowgage$flowdata$flow/myflowgage$area/10^3
@@ -44,7 +44,7 @@ myflowgage$flowdata$Qmm = myflowgage$flowdata$flow/myflowgage$area/10^3
 # parsing methods most old people use.
 #
 # Sorry, but there is even an easier way!
-?FillMissWX()
+FillMissWX()
 WXData=FillMissWX(declat=myflowgage$declat, declon=myflowgage$declon,
                   StnRadius=30,minstns=10,date_min="2010-01-01",
                   date_max="2023-02-01",targElev=1,
@@ -59,9 +59,9 @@ MinTempCol <- "#0000ff"
       
       coeff=1
       p1= ggplot(BasinData, aes(x=date)) +
-        geom_line( aes(y=MaxTemp), linewidth=1, color=MaxTempCol) + 
-        geom_line( aes(y=MinTemp), linewidth=1, color=MinTempCol) + 
-        geom_line( aes(y=Qmm), linewidth=1, color=QCol) +
+        geom_line( aes(y=MaxTemp), linewidth=0.5, color=MaxTempCol) + 
+        geom_line( aes(y=MinTemp), linewidth=0.5, color=MinTempCol) + 
+        geom_line( aes(y=Qmm), linewidth=0.5, color=QCol) +
         scale_y_continuous(
           # Features of the first axis
           name = "Temp(C)",
@@ -147,11 +147,6 @@ zoomext=rbind(zoomext,zoomext+res(mydem)*100)
 zoomext=rbind(zoomext,zoomext-res(mydem)*100)
 zoomext=SpatialPoints(zoomext,proj4string = crs_utm)  
 zoom(mydem,ext=zoomext)
-zoomext2=myflowgage$gagepoint_utm@coords
-zoomext2=rbind(zoomext2,zoomext2+res(mydem)*10)
-zoomext2=rbind(zoomext2,zoomext2-res(mydem)*10)
-zoomext2=SpatialPoints(zoomext2,proj4string = crs_utm)  
-zoom(mydem,ext=zoomext2)
 plot(bboxpts,add=T)
 plot(pourpoint,add=T,col="red")
 
@@ -180,34 +175,34 @@ if( ! grepl("~/src/TauDEM/bin",old_path)){
  plot(z)
  
  # Pitremove
- system("mpiexec -n 2 pitremove -z mydem.tif -fel mydemfel.tif")
+ system("mpiexec -n 16 pitremove -z mydem.tif -fel mydemfel.tif")
  fel=raster("mydemfel.tif")
  plot(fel)
  
  
  # D8 flow directions
- system("mpiexec -n 2 d8flowdir -p mydemp.tif -sd8 mydemsd8.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
+ system("mpiexec -n 16 d8flowdir -p mydemp.tif -sd8 mydemsd8.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
  p=raster("mydemp.tif")
  plot(p)
  sd8=raster("mydemsd8.tif")
  plot(sd8)
  
  # Contributing area
- system("mpiexec -n 2 aread8 -p mydemp.tif -ad8 mydemad8.tif")
+ system("mpiexec -n 16 aread8 -p mydemp.tif -ad8 mydemad8.tif")
  ad8=raster("mydemad8.tif")
  plot(log(ad8))
- zoom(log(ad8))
+ zoom(log(ad8),ext=zoomext2)
  plot(pourpoint,add=T)
  
  
  # Grid Network 
- system("mpiexec -n 2 gridnet -p mydemp.tif -gord mydemgord.tif -plen mydemplen.tif -tlen mydemtlen.tif")
+ system("mpiexec -n 16 gridnet -p mydemp.tif -gord mydemgord.tif -plen mydemplen.tif -tlen mydemtlen.tif")
  gord=raster("mydemgord.tif")
  plot(gord)
- zoom(gord,ext=zoomtext)
+ zoom(gord,ext=zoomext)
  
  # DInf flow directions
- system("mpiexec -n 2 dinfflowdir -ang mydemang.tif -slp mydemslp.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
+ system("mpiexec -n 16 dinfflowdir -ang mydemang.tif -slp mydemslp.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
  ang=raster("mydemang.tif")
  plot(ang)
  slp=raster("mydemslp.tif")
@@ -215,13 +210,13 @@ if( ! grepl("~/src/TauDEM/bin",old_path)){
  
  
  # Dinf contributing area
- system("mpiexec -n 2 areadinf -ang mydemang.tif -sca mydemsca.tif")
+ system("mpiexec -n 16 areadinf -ang mydemang.tif -sca mydemsca.tif")
  sca=raster("mydemsca.tif")
  plot(log(sca))
  zoom(log(sca))
  
  # Threshold
- system("mpiexec -n 2 threshold -ssa mydemad8.tif -src mydemsrc.tif -thresh 100")
+ system("mpiexec -n 16 threshold -ssa mydemad8.tif -src mydemsrc.tif -thresh 100")
  src=raster("mydemsrc.tif")
  plot(src)
  zoom(src)
@@ -233,7 +228,7 @@ if( ! grepl("~/src/TauDEM/bin",old_path)){
             driver="ESRI Shapefile", overwrite_layer=TRUE)
  
  # Move Outlets
- system("mpiexec -n 2 moveoutletstostrm -p mydemp.tif -src mydemsrc.tif -o approxoutlets.shp -om outlet.shp")
+ system("mpiexec -n 16 moveoutletstostrm -p mydemp.tif -src mydemsrc.tif -o approxoutlets.shp -om outlet.shp")
  
  approxpt=readOGR("approxoutlets.shp")
  plot(approxpt,add=T, col="blue")
@@ -248,24 +243,124 @@ if( ! grepl("~/src/TauDEM/bin",old_path)){
  points(outpt$shp[2],outpt$shp[3],pch=19,col=2)
  points(approxpt$shp[2],approxpt$shp[3],pch=19,col=4)
  
- zoom(src)
+ zoom(src,ext=zoomext2)
  
  
  # Contributing area upstream of outlet
- system("mpiexec -n 2 aread8 -p mydemp.tif -o outlet.shp -ad8 mydemssa.tif")
+ system("mpiexec -n 16 aread8 -p mydemp.tif -o outlet.shp -ad8 mydemssa.tif")
  ssa=raster("mydemssa.tif")
  plot(ssa) 
+ zoom(ssa)
  
  
  # Threshold
- system("mpiexec -n 2 threshold -ssa mydemssa.tif -src mydemsrc1.tif -thresh 2000")
+ system("mpiexec -n 16 threshold -ssa mydemssa.tif -src mydemsrc1.tif -thresh 2000")
  src1=raster("mydemsrc1.tif")
  plot(src1)
  zoom(src1)
  
  # Stream Reach and Watershed
- system("mpiexec -n 2 streamnet -fel mydemfel.tif -p mydemp.tif -ad8 mydemad8.tif -src mydemsrc1.tif -o outlet.shp -ord mydemord.tif -tree mydemtree.txt -coord mydemcoord.txt -net mydemnet.shp -w mydemw.tif")
+ system("mpiexec -n 16 streamnet -fel mydemfel.tif -p mydemp.tif -ad8 mydemad8.tif -src mydemsrc1.tif -o outlet.shp -ord mydemord.tif -tree mydemtree.txt -coord mydemcoord.txt -net mydemnet.shp -w mydemw.tif")
  plot(raster("mydemord.tif"))
  zoom(raster("mydemord.tif"))
  plot(raster("mydemw.tif"))
 
+ 
+ ####################### StREAM LAB########################################################
+ 
+ latsl <- 37.20776768624577
+ longsl <- -80.44703835286296
+ 
+ trunc((180+longsl)/6+1)
+ projsl_utm = paste0("+proj=utm +zone=", trunc((180+longsl)/6+1), " +datum=WGS84 +units=m +no_defs")
+ print(projsl_utm)
+ 
+ # Lat/Lon (_ll) is much easier!
+ projsl_ll = "+proj=longlat"
+ 
+ # Now we will build our proj4strings which define our “Coordinate 
+ # Reference Systems” or CRS in future geographic manipulations. 
+ crs_sl_ll=CRS(projsl_ll)
+ crs_sl_utm=CRS(projsl_utm)
+ print(crs_sl_ll)
+ print(crs_sl_utm)
+ sl_latlon <- cbind(longsl,latsl)
+ streamlab_ll <- SpatialPoints(sl_latlon)
+ proj4string(streamlab)=projsl_ll
+ streamlab_utm=spTransform(myflowgage$gagepoint_ll,crs_sl_utm)
+ # Open up maps.google.com to guesstimate area/lengths
+ sl_url=paste0("https://www.google.com/maps/@",
+            latsl,",",longsl,",18z")
+ browseURL(sl_url)
+ # We are going to over estimate our area
+ sqrt(myflowgage$area)   # guestimating square watershed
+ # For our search we are going to multiply the area by 6 and
+ # to get the distance
+ 
+ 
+ sl_searchlength=5000 
+ sl_pourpoint=SpatialPoints(streamlab_utm@coords,proj4string = crs_sl_utm)
+ sl_bboxpts=streamlab_utm@coords
+ sl_bboxpts=rbind(sl_bboxpts,sl_bboxpts+sl_searchlength)
+ sl_bboxpts=rbind(sl_bboxpts,sl_bboxpts-sl_searchlength)
+ sl_bboxpts
+ sl_bboxpts=rbind(sl_bboxpts,c(min(sl_bboxpts[,1]),max(sl_bboxpts[,2])))
+ sl_bboxpts=rbind(sl_bboxpts,c(max(sl_bboxpts[,1]),min(sl_bboxpts[,2])))
+ sl_bboxpts
+ sl_bboxpts=SpatialPoints(sl_bboxpts,proj4string = crs_sl_utm)
+ # From Lab04, get your DEM
+ sl_dem=get_aws_terrain(locations=sl_bboxpts@coords, 
+                       z = 12, prj = projsl_utm,src ="aws",expand=1)
+ res(sl_dem)
+ plot(sl_dem)
+ plot(sl_bboxpts,add=T)
+ plot(sl_pourpoint,add=T,col="red")
+ 
+ # Write our raster to a geotiff file that can be used with
+ # OS level hydrological models 
+ writeRaster(sl_dem,filename = "sl_dem.tif",overwrite=T)
+ # Our quick intro to terminal where the cloud offerings are usually Linux
+ # ls; cd ~; pwd;  # Linux/Mac 
+ # dir; cd ; # Windows
+ 
+ ###### CHANGRING PLOT FRAME
+ sl_zoom=streamlab_utm@coords
+ sl_zoom=rbind(sl_zoom,sl_zoom+res(sl_dem)*100)
+ sl_zoom=rbind(sl_zoom,sl_zoom-res(sl_dem)*100)
+ sl_zoom=SpatialPoints(sl_zoom,proj4string = crs_sl_utm)  
+ zoom(sl_dem,ext=sl_zoom)
+ sl_zoom2=streamlab_utm@coords
+ sl_zoom2=rbind(sl_zoom2,sl_zoom2+res(sl_dem)*10)
+ sl_zoom2=rbind(sl_zoom2,sl_zoom2-res(sl_dem)*10)
+ sl_zoom2=SpatialPoints(sl_zoom2,proj4string = crs_sl_utm)  
+ zoom(sl_dem,ext=sl_zoom2)
+ plot(bboxpts,add=T)
+ plot(sl_pourpoint,add=T,col="red")
+
+ 
+ rm("old_path")
+ old_path <- Sys.getenv("PATH")
+ old_path
+ if( ! grepl("~/src/TauDEM/bin",old_path)){
+   Sys.setenv(PATH = paste(old_path,
+                           paste0(Sys.getenv("HOME"),"/src/TauDEM/bin"), 
+                           sep = ":"))
+ }
+ 
+ system("mpirun aread8")
+ 
+ #####Pulled from https://hydrology.usu.edu/taudem/taudem5/TauDEMRScript.txt
+ 
+ setwd(datadir)
+ 
+ z=raster("sl_dem.tif")
+ plot(z)
+ zoom(z,ext=sl_zoom)
+ 
+ # Pitremove
+ system("mpiexec -n 16 pitremove -z sl_dem.tif -fel sldemfel.tif")
+ fel_sl=raster("sldemfel.tif")
+ plot(fel_sl -z)
+ plot(sl_bboxpts,add=T)
+ plot(sl_pourpoint,add=T,col="red")
+ 
